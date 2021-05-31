b946aa1a3168bf05bbdee67e58491d1f04e844f7
tajudeen <tajudeenomotayo3@gmail.com>
2021-05-31 11:56:07 -0700

    javascriptHistory

---
+++ javascript/javascriptHistoryAndSetup.js
@@ -0,0 +1,22 @@
+// Solution
+// 1. JavaScript is more of a programming language which implement 
+//ECMAScript standard. In JavaScript community both can be interchange,
+// ECMAScript or ES6 also called ECMA2015 is the standard at which
+// JavaScript language is written.
+// 2. Brendan Eich is a developer that started writing code with 
+//lisp language at the age of 11. He worked as a developer for Netscape 
+//now mozilla, where he develop a programming language called JavaScript
+// under 10 days.
+// 3. Chrome console can be display by pressing
+// - 	F12
+// -	CRTL Shift and J
+// -	Using developer tools at the right top of the chrome browers
+// -	Clicking on inspection after right clicking on the page and 
+//navigating to console.
+// Chrome console can be hide by repeating the  two steps above
+// 4.  Prompt is a JavaScript method that prompt user to enter a 
+//value as input. User must input a value in the display box and click ok or cancel before it can allow other interaction with the page.
+// Confirm make the user to accept what is displayed by clicking ok or
+// reject by click cancel. 
+
+
