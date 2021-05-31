2bbe21a2d4797af9923e0ef5287f1c6ab5d5ce79
tajudeen <tajudeenomotayo3@gmail.com>
2021-05-31 11:42:06 -0700

    variable and Primitive solution

---
+++ javascript/variableAndPrimitive.js
@@ -0,0 +1,16 @@
+const  name = 'ABDULAZEEZ';
+const dayOfBirth = 3;
+//2.
+ //The last assigned variable take preference over others
+//3.
+  let color = prompt("What is your favourite color");                       
+ console.log( color + ' That is a nice color');
+//4. 
+ const boy =  'Very honest';
+const girl = " very beautiful"
+// 5. Null is a type of data that can is explicitly assigned to a variable. A variable can be assigned null as a value. when typeof null is used output object.
+// Undefined is when a variable is declared but not assigned any value
+// Undefined is a type of  data that may not be explicitly assigned to a variable
+// typeof Undefined output Undefined.
+// 6. NaN means N ot a number. they typeof NaN is number.
+// 7. The value of thing id undefined.
